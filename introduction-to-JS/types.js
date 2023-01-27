715c6aee463c2ee7e7909a57add114f107fc8bb2
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 12:46:51 +0600

    types.js code update

---
+++ introduction-to-JS/types.js
@@ -0,0 +1,11 @@
+var price = 100;
+console.log(typeof price);
+
+var price = '100';
+console.log(typeof price);
+
+var isHappy = true;
+console.log(typeof isHappy);
+
+var store;
+console.log(typeof store);
