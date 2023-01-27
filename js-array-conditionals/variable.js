c68a87b3bbe861ed214bf1bb52fd622c5510a675
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 20:46:47 +0600

    js array practice file added

---
+++ js-array-conditionals/variable.js
@@ -0,0 +1,32 @@
+// number type variable
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Rohmot Miah";
+
+// boolean type variable
+var isSingle = true;
+
+//:::: Previous Learning Recap :::::::
+// two types of number: Integer, 120,
+// float, decimal: 3.65,
+
+//variable naming conversion: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or space
+ * 3. no quote
+ * 4. no keyword
+ * 5. cannot start with a number
+ * 6. cannot contain dash
+ * 7. should use camelCase
+ */
+
+// Math Operations: +, -, *, /
+// Shorthand: +=, -=, ++, --
+
+//convert: parseInt, parseFloat
+
+// toFixed
+
+// remainder: %
