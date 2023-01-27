540db0ebaa7fbcdcf639e4224277d25e9d98680a
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 11:24:37 +0600

    string concetenation code updated

---
+++ introduction-to-JS/string-concatenation.js
@@ -0,0 +1,10 @@
+var price1 = 20;
+var price2 = 10;
+
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+var firstName = 'Metropolitan';
+var secondName = 'University';
+var fullName = firstName + ' ' + secondName;
+console.log(fullName);
