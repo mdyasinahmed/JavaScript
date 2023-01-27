7712660544bfe312c3da03dcad4ba5c865048d29
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 20:56:47 +0600

    js array code updated

---
+++ js-array-conditionals/array.js
@@ -0,0 +1,13 @@
+// put values
+var friendAge = [18, 21, 23, 22, 20];
+var priceFee = [500, 200, 4000, 100];
+var names = ['Rohim','Korim', 'Rohmot', 'Borkot'];
+var vowels = ["a", "e", "i", "o", "u"];
+
+// print array
+console.log(priceFee);
+
+// length of array
+console.log(friendAge.length);
+console.log(priceFee.length);
+console.log(vowels.length);
