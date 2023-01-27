3d1dac1822f79a08115beecf3085f561f67e86cb
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 11:15:41 +0600

    shorthand code updated

---
+++ introduction-to-JS/shorthand.js
@@ -0,0 +1,24 @@
+// variable declaration
+var tomatoPrice = 25;
+
+// variable value update
+tomatoPrice = 30;
+
+// add something to previous value
+var price1 = 50;
+var price2 = 10;
+
+// set a new value
+// price1 = 35;
+
+// add something to the existing value
+// price1 = price1 + 9;
+price1 += 10;
+price1 -= 10;
+price1 *= 10;
+price1 /= 10;
+// += means add something to the existing value
+console.log(price1);
+
+// ++ : to increasing just 1
+// -- : to decrease just 1
