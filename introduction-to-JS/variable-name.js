1328b30a4a1b2eafb42517e332c430972b2ef1b2
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 10:41:18 +0600

    variable-name code added

---
+++ introduction-to-JS/variable-name.js
@@ -0,0 +1,28 @@
+
+// 1. variable name can't be any keywords
+var false = 96;
+var return = true;
+
+// 2. variable name has to be in one word. No space
+var my home address = "New California";
+
+// 3. variable name can't have quotation;
+var 'name' = 'Tom Tom';
+
+// 4. variable name cannot start with a number but ends with a number
+var 71cetona = 1971;
+var cetona71 = 1971;
+
+// 5. How to use long names
+// cannot use dash
+var user-name = "mohammed ahmed";
+
+var user_name = "mohammed ahmed"; // use underscore
+var user_name_here = "mohammed ahmed"; // use snake case
+//mostly used
+var userName = "mohammed ahmed"; // use camel case
+var UserNameHere = "mohammed ahmed"; // use pascal case
+
+// variable name is case sensitive
+var person = 25;
+var Person = 35;
