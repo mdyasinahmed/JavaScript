25d4d9aa2f4e04539a75af18468bc12be781b73d
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 19:13:54 +0600

    remainder js file added

--- introduction-to-JS/types.js
+++ introduction-to-JS/types.js
@@ -9,3 +9,11 @@ console.log(typeof isHappy);
 
 var store;
 console.log(typeof store);
+
+// watch on youtube- JS complexity
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1)
+console.log(sum)
