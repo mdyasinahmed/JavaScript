ae7e12fc9db208e64dd50c0e9fb72f78afd07eca
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 10:59:40 +0600

    js operations file added

---
+++ introduction-to-JS/operation.js
@@ -0,0 +1,25 @@
+var onionPrice = 20;
+var eggPrice = 10;
+// console.log(onionPrice);
+// console.log(eggPrice);
+
+// add
+// console.log(onionPrice+eggPrice);
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+//subtract
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+// multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice*quantity;
+// console.log(totalCost);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
