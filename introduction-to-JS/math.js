a9a8b7077497c7fc4d6acfd19247fc82b05229ad
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-27 10:23:29 +0600

    basic files added

---
+++ introduction-to-JS/math.js
@@ -0,0 +1,16 @@
+/**
+ * 1. number 225 44 2 4
+ * 2. integer: odd, even, 2, 5
+ * 3. float: 2.5 6.7
+ * 4. operation: addition, multiplication, division, reminder
+ * 5. percent
+ * 6. average
+ * 7. comparison: >, <, ==, !=, >=, <=
+ * 8. oikik: 
+ * 9. greater, less etc.
+ * 10. series
+ * :::::Extra:::::
+ * 1. prime number
+ * 2. permutation
+ * 3. Area:
+ */
---
+++ introduction-to-JS/variable.js
@@ -0,0 +1,14 @@
+// vary + able = variable
+
+// numeric
+var price = 11;
+var age = 17;
+var temperature = 38;
+
+// string
+var name = "muhammad ahmed" ;
+var address = 'sylhet' ;
+
+//Boolean
+var work = false;
+var sleep = true;
