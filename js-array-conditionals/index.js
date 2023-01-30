c43a98f8e756ca63d7e925cd24081f0892924bcb
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-30 15:39:57 +0600

    js more practice file added

---
+++ js-array-conditionals/index.js
@@ -0,0 +1,15 @@
+var numbers = [45, 50, 55, 60, 65];
+
+// 1. get element by index
+console.log(numbers[0]);
+var element = numbers[4];
+console.log(element);
+
+// 2. set element value by index
+numbers[1] = 55;
+numbers[3] = 66;
+console.log(numbers);
+
+// 3. find index of index
+var positionIndex = numbers.indexOf(66)
+console.log(positionIndex);
---
+++ js-array-conditionals/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+var numbers = [45, 50, 55, 60, 65];
+
+// // get element by index
+// // console.log(numbers[0]);
+// var element = numbers[4];
+// console.log(element);
+// numbers[0] = 44;
+// numbers[1] = 54;
+// numbers[2] = 54;
+// numbers[3] = 54;
+// numbers[4] = 54;
