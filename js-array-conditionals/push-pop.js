0fa21eed52a316afc1211d00ae8fc74ac6963b1b
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-30 15:48:21 +0600

    js push and pop code added

---
+++ js-array-conditionals/push-pop.js
@@ -0,0 +1,14 @@
+var numbers = [15, 30, 53, 75];
+// Use push to add a last element in array 
+numbers.push(85);
+console.log(numbers);
+
+var friends = ['balam','kalam','salam'];
+friends.push('gelam');
+friends.push('pelam');
+console.log(friends);
+
+// use pop to remove last element in array
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
