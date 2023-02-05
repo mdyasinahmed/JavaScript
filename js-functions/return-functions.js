44b6231511bc7859596272ecad9b675b0d10c240
mdyasinahmed <yasinahmed7875@gmail.com>
2023-02-06 02:55:29 +0600

    more js code added

---
+++ js-functions/return-functions.js
@@ -0,0 +1,15 @@
+// program to add two numbers
+// declaring a function
+function add(a, b) {
+    return a + b;
+}
+
+// take input from the user
+let number1 = 5;
+let number2 = 6;
+
+// calling function
+let result = add(number1,number2);
+
+// display the result
+console.log("The sum is " + result);
---
+++ js-functions/tempCodeRunnerFile.js
@@ -0,0 +1,64 @@
+// function inchToFeet()
+// { 
+//     var feet = inch/12; feet; 
+// }
+// var number = 6, i, factorial;
+
+// for(i = number;i>0;i--)
+// {
+//     factorial = number - 1;
+//     factorial *= i;
+    
+// }
+//   console.log(factorial);
+// let i;
+// for(i = 10; i<=1; i--){ 
+// }
+// console.log(i);
+// function print(a, b, c){
+//     return a+2;
+//     return a*b;
+//     return b*c+a;
+//   }
+//   console.log(print(1,2,3));
+// console.log(Math.round(Math.random()*10));
+// console.log(Math.floor(13.67));
+// const number = -78; 
+// const answer = Math.abs(number); 
+// console.log(answer);
+
+// function add(a, b){
+//     return a + b;
+//   }
+//   console.log(add("adam" + "eve"))
+
+// let a = 5, b = 3;
+// if(a != b){
+//     console.log('Hi');
+// }
+
+// let x = 4, y = 5;
+// console.log(Math.min(x,y));
+
+// let sum=0; 
+// for( let i = 0; i<=3;i++){ 
+// sum = sum + i; 
+// }
+// console.log(sum);
+
+// let myString = "Hello World!";
+// let length = myString.length;
+// console.log(length);
+
+// var student = { name : "Yo Mama", age : 17 };
+// console.log(student);
+
+// delete student.age;
+// console.log(student);
+
+let arr = [1, 2, 3, 4];
+console.log(arr);
+let length = arr.length;
+console.log(length);
+arr[arr.length-1] = 5;
+console.log(arr);
