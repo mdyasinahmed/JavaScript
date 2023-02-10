ecc5b8397303b7daf6d3dc8fb25dc556864e05bd
mdyasinahmed <yasinahmed7875@gmail.com>
2023-02-10 10:53:22 +0600

    understanding recursion file added

---
+++ js-recursion/simple.js
@@ -0,0 +1,15 @@
+// let sum = 0;
+// for(let i = 0; i<=5; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for(let i = 5; i >=1; i--){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+function sum(i){
+
+}
