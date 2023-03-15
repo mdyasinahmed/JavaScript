ba1623b23540ac040d837931f43b56a1b93cc965
mdyasinahmed <yasinahmed7875@gmail.com>
2023-03-15 11:16:08 +0600

    js-comment box code added

---
+++ js-Event/events.js
@@ -0,0 +1,24 @@
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+    document.body.style.color = 'white';
+}
+function makeBlack(){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = "white";
+}
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+    document.body.style.color = "white";
+}
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+    document.body.style.color = "white";
+}
+function makeOrange(){
+    document.body.style.backgroundColor = 'orange';
+    document.body.style.color = "white";
+}
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+    document.body.style.color = "white";
+}
